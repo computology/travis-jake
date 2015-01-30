614a8dfc3d3e2723e194146825f4a628475a4be3
capotej <jcapote@gmail.com>
2015-01-30 11:21:09 -0800

    initial commit

---
+++ jake-1.0/jake.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* add a comment just to make this different for testing purposes */
+
+int
+main (int argc, char *argv[])
+{
+      printf("as it so happens, jake douglas is a very nice young man.\n");
+      return 0;
+}
